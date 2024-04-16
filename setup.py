5aa640d264103002a76c20690df1a7d938138bd3
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-04-16 11:21:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup
+
+setup(
+   name='mtraker',
+   version='1.0',
+   description='Provides a decorator for memory usage tracking. The part of FOSS course.',
+   license='MIT',
+   author='Kirilligu',
+   author_email='k.degtyarev4@mail.ru',
+   url='https://github.com/Kirilligu/setup',
+   packages=['mtracker'],
+   install_requires=[], # it is empty since we use standard python library
+   extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+   },
+   python_requires='>=3',
+)
 
