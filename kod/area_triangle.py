ec919c1cd772464a510883cb64f40a11b01a6fbd
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-04-16 11:08:51 +0800

    Update area_triangle.py

--- kod/area_triangle.py
+++ kod/area_triangle.py
@@ -1,1 +1,8 @@
+def calculate_triangle_area(a, b, c):
+    s = (a + b + c) / 2
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    return area
+
+triangle_area = calculate_triangle_area(3, 4, 5)
+print("Площадь треугольника равна:", triangle_area)
 
