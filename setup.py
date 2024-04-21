9eb7f35ce329dd3e201e044ba0ad5cfb51b8d455
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-04-21 19:04:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-   name='kod',
+   name='triangle_area_calculator',
    version='1.0',
    description='a utility that calculates the area of a triangle',
    license='MIT',
    author='Kirilligu',
    author_email='k.degtyarev4@mail.ru',
    url='https://github.com/Kirilligu/setup',
-   packages=['mtracker'],
+   packages=[''],
    install_requires=[], # it is empty since we use standard python library,
    extras_require={
         'test': [
