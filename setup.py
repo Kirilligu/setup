a54cf29a6ea12cf261588cd9f7c8435729305752
Kirilligu <149255706+Kirilligu@users.noreply.github.com>
2024-04-21 18:27:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
    name='kod',
    version='1.0',
-   description='Provides a decorator for memory usage tracking. The part of FOSS course.',
+   description='a utility that calculates the area of a triangle',
    license='MIT',
    author='Kirilligu',
    author_email='k.degtyarev4@mail.ru',
    url='https://github.com/Kirilligu/setup',
    packages=['mtracker'],
-   install_requires=[numpy],
+   install_requires=[], # it is empty since we use standard python library,
    extras_require={
         'test': [
             'pytest',
